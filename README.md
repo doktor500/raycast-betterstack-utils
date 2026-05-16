12b6f5ee977c50079064803943f2f7d3836bc058
David Molinero <doktor500@users.noreply.github.com>
2026-05-16 18:47:47 +0100

    Initial commit
    Initial commit
    
    Initial commit
    
    Initial commit
    
    Update readme
    
    Remove pnpm files
    
    Update readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# raycast-betterstack-utils
+
+[Raycast](https://www.raycast.com/) extension with some utilities for BetterStack
+
+![betterstack.png](docs/betterstack.png)
---
+++ raycast-env.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** API Token - Your BetterStack API token */
+  "apiToken": string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `on-call-schedule` command */
+  export type OnCallSchedule = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `on-call-schedule` command */
+  export type OnCallSchedule = {}
+}
+
