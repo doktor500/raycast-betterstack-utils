7c2624309a16244f4057f53a016c8a5731ac94d7
David Molinero <doktor500@users.noreply.github.com>
2026-06-27 08:43:57 +0100

    Assign shortchut

--- src/ui/action-panel/actions/open-schedule-in-browser-action.tsx
+++ src/ui/action-panel/actions/open-schedule-in-browser-action.tsx
@@ -5,5 +5,5 @@ type OpenScheduleInBrowserActionProps = {
 };
 
 export function OpenScheduleInBrowserAction({ url }: OpenScheduleInBrowserActionProps) {
-  return <Action.OpenInBrowser title="Open Schedule in Browser" url={url} />;
+  return <Action.OpenInBrowser title="Open Schedule in Browser" url={url} shortcut={{ modifiers: ["cmd", "shift"], key: "o" }} />;
 }
